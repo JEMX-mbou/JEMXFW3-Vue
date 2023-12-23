b67ef51b2dd930a7de257275314f0d00a85d73ea
Jesse Malotaux <jemx@mboutrecht.nl>
2023-12-23 17:58:54 +0100

    Test pages update: Changed Homepage and Aboutpage.
    This is definitely not done.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,63 @@
 <template>
   <main id="about" class="container">
     <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere nulla harum voluptas dolor vel
+      perspiciatis. Dolor dicta nemo ratione labore cum eius autem ad minus, eaque quasi reiciendis
+      soluta atque.
+    </p>
+    <p>
+      Officiis aspernatur ducimus omnis, tempora sapiente veritatis quasi asperiores corporis et
+      nisi. Quidem fugiat harum autem explicabo, animi ut aliquam voluptatum at corrupti blanditiis
+      eaque ipsa inventore neque fugit fuga!
+    </p>
+    <p>
+      Corrupti possimus voluptate sit repudiandae ad cumque sunt, laborum ullam placeat. Aperiam
+      natus dolorem ab animi provident? Nostrum doloremque soluta, dolor fugit reprehenderit
+      assumenda molestiae autem amet quod eligendi impedit?
+    </p>
+    <p>
+      Quidem voluptates dicta dolor atque temporibus. Dolor fugit nulla placeat consequatur
+      molestias nam sed ullam aperiam? Inventore, qui fugit aperiam totam atque officia dolorum
+      culpa, voluptatibus nostrum eius tempora? Assumenda.
+    </p>
+    <p>
+      Ut, cum itaque! Atque non nemo quisquam, soluta obcaecati dolorum optio voluptas magnam
+      numquam provident neque porro eaque tempora voluptatibus maiores dicta, similique ratione
+      natus minus nostrum? Id, consequatur eos!
+    </p>
+    <p>
+      Aut blanditiis beatae consectetur recusandae minus esse ad hic voluptatem est perspiciatis
+      modi sapiente optio alias omnis itaque, quia quibusdam ipsam explicabo adipisci mollitia
+      veniam iure aliquam quasi! Eius, facilis.
+    </p>
+    <p>
+      Earum, consequatur nulla. Ratione aliquam voluptatibus facere natus alias quam saepe quod
+      explicabo fugiat sit tenetur architecto nesciunt pariatur doloremque enim deserunt corporis
+      totam voluptates neque, atque quibusdam tempore magnam!
+    </p>
+    <p>
+      Assumenda, doloribus? Non iure dolor, expedita molestiae, eius nesciunt accusantium soluta
+      ipsum ab est ducimus quia molestias corporis accusamus error fuga incidunt veniam sed
+      doloribus! Ullam voluptas nisi corporis ipsa.
+    </p>
+    <p>
+      Soluta inventore eaque itaque adipisci, sed autem ipsam numquam reiciendis consectetur
+      mollitia consequuntur laboriosam labore facere porro quia eveniet animi molestiae! Fugit
+      corporis magnam tempore delectus asperiores tenetur? Magnam, neque.
+    </p>
+    <p>
+      Quo accusantium dolores deleniti iure. Sequi, laborum temporibus voluptatibus recusandae hic,
+      odio, eaque sint adipisci eligendi neque est dolorem molestiae dicta architecto tempora
+      placeat. Suscipit quaerat praesentium nulla quidem at.
+    </p>
   </main>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+  height: 200vh;
+}
+</style>
